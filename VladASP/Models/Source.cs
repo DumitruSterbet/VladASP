dbbe9de37b1958a24dedf229fa117bfc3ec178c3
dimasterbet <ssterbet@list.ru>
2021-04-21 18:26:16 +0300

    Revert "Revert "Add project files.""
    This reverts commit f93c8d5e564948c8cee9b2dd1c5ae3ba1e6c9f50.

---
+++ VladASP/Models/Source.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VladASP.Models
+{
+    public class Source
+    {
+        public int Id { set; get; }
+        public string Name { set; get; }
+        public string Cod { set; get; }
+     
+
+    }
+}
